6a6f8d3df55304d4d9db03017ea998dafebc326f
Lázaro Josué <jcslazaro@yahoo.com.br>
2023-07-28 21:47:36 -0300

    Adicionado front-end

--- ControleDeContatos/Controllers/ContatoController.cs
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -12,5 +12,20 @@ namespace ControleDeContatos.Controllers
         {
             return View();
         }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
     }
 }
